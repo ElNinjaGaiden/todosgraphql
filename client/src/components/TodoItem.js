dcebb46c3e4c438d8cce4190be6caccbe6f00d00
Diego García <diego.garcia.segura@gmail.com>
2017-10-05 17:30:28 -0600

    Big fixes on todo item component

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import Avatar from 'material-ui/Avatar';
@@ -25,62 +25,73 @@ const style = {
     }
 };
 
-const TodoItem = ({ todo, onClick }) => {
+class TodoItem extends Component {
 
-    const handleClick = (event) => {
-        if(typeof onClick === 'function') {
-            onClick(todo);
+    constructor(props) {
+        super(props);
+        this.state = {
+            avatar: this.resolveAvatar()
+        };
+    }
+
+    handleClick = (event) => {
+        if(typeof this.props.onClick === 'function') {
+            this.props.onClick(this.props.todo);
         }
     };
 
-    const resolveAvatar = () => {
-        if(todo.userByOwnerid) {
-            const rand = Math.floor(Math.random() * 200) + 1;
+    resolveAvatar = () => {
+        if(this.props.todo.userByOwnerid && !this.props.todo.avatar) {
+            const rand = Math.floor(Math.random() * 100) + 1;
             const genderFlag = Math.random() >= 0.5;
             const gender = genderFlag ? 'women' : 'men';
-            return `https://randomuser.me/api/portraits/${gender}/${rand}.jpg`;
+            const avatar = `https://randomuser.me/api/portraits/${gender}/${rand}.jpg`;
+            return avatar;
         }
         return noAvatar;
     }
 
-    const priorityColor = todo.priorityByPriorityid.id === 1 ? greenA700 : (todo.priorityByPriorityid.id === 2 ? yellow500 : red500);
-    return (
-        <div>
-            <ListItem onClick={handleClick.bind(this)} key={todo.id} 
-                primaryText={<p className={'todo-details-title'}>{todo.title}</p>} 
-                leftAvatar={<Avatar src={resolveAvatar()} style={style.todo.details.avatar} />}
-                innerDivStyle={style.todo.details.innerDiv}
-                secondaryText={
-                <div style={style.todo.details}>
-                    <p className={'todo-details-description'}>{todo.description}</p>
-                    <div style={style.todo.details.fields} className={'todo-details-fields'}>
-                        <div className={'todo-details-fields-col'}>
-                            <div className={'todo-details-field-header'}>Status:</div>
-                            <div>{todo.todostatusByStatusid.name}</div>
-                            <div className={'todo-details-field-header'}>Priority:</div>
-                            <div style={{color: priorityColor}}>{todo.priorityByPriorityid.name}</div>
-                        </div>
-                        <div className={'todo-details-fields-col'}>
-                            <div className={'todo-details-field-header'}>Owner:</div>
-                            {
-                                todo.userByOwnerid ? <div>{todo.userByOwnerid.firstname} {todo.userByOwnerid.lastname}</div> : <div>Not assigned</div>
-                            }
-                            <div className={'todo-details-field-header'}>Due date:</div>
-                            <div >{todo.duedate}</div>
-                        </div>
-                        <div className={'todo-details-fields-col'}>
-                            <div className={'todo-details-field-header'}>Created by:</div>
-                            <div>{todo.userByCreatorid.firstname} {todo.userByCreatorid.lastname}</div>
-                            <div className={'todo-details-field-header'}>Created on:</div>
-                            <div>{new Date(todo.createdon).toDateString()}</div>
+    render = () => {
+        const { todo } = this.props;
+        const priorityColor = todo.priorityByPriorityid.id === 1 ? greenA700 : (todo.priorityByPriorityid.id === 2 ? yellow500 : red500);
+        return (
+            <div>
+                <ListItem onClick={this.handleClick.bind(this)} key={todo.id} 
+                    primaryText={<p className={'todo-details-title'}>{todo.title}</p>} 
+                    leftAvatar={<Avatar src={this.state.avatar} style={style.todo.details.avatar} />}
+                    innerDivStyle={style.todo.details.innerDiv}
+                    secondaryText={
+                    <div style={style.todo.details}>
+                        <p className={'todo-details-description'}>{todo.description}</p>
+                        <div style={style.todo.details.fields} className={'todo-details-fields'}>
+                            <div className={'todo-details-fields-col'}>
+                                <div className={'todo-details-field-header'}>Status:</div>
+                                <div>{todo.todostatusByStatusid.name}</div>
+                                <div className={'todo-details-field-header'}>Priority:</div>
+                                <div style={{color: priorityColor}}>{todo.priorityByPriorityid.name}</div>
+                            </div>
+                            <div className={'todo-details-fields-col'}>
+                                <div className={'todo-details-field-header'}>Owner:</div>
+                                {
+                                    todo.userByOwnerid ? <div>{todo.userByOwnerid.firstname} {todo.userByOwnerid.lastname}</div> : <div>Not assigned</div>
+                                }
+                                <div className={'todo-details-field-header'}>Due date:</div>
+                                <div >{todo.duedate}</div>
+                            </div>
+                            <div className={'todo-details-fields-col'}>
+                                <div className={'todo-details-field-header'}>Created by:</div>
+                                <div>{todo.userByCreatorid.firstname} {todo.userByCreatorid.lastname}</div>
+                                <div className={'todo-details-field-header'}>Created on:</div>
+                                <div>{new Date(todo.createdon).toDateString()}</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                } >
-            </ListItem>
-            <Divider inset={true} />
-        </div>
-    )
-};
+                    } >
+                </ListItem>
+                <Divider inset={true} />
+            </div>
+        )
+    }
+}
 
 export default TodoItem;
